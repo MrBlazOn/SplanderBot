56d26be817eb8436c5c8244dc25d2f350dc93753
Egor Iskrenkov <mrblazon@gmail.com>
2018-10-14 23:09:56 +0300

    unsplash func fix

--- commands.py
+++ commands.py
@@ -5,6 +5,7 @@ from unsplash.auth import Auth
 client_id = "1904d81ebf366e14d1d5ce804d0f268a29126d9a18d27703ab554b92570fffe6"
 client_secret = "fc68dbac271c9450d7d157daac2ce9bb633088cc3447f1f228b74597205d842d"
 redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+code = ''
 
 auth = Auth(client_id, client_secret, redirect_uri, code = code)
 unsplash_api = Api(auth)
