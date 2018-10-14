f6eae88b31a31126152b9d41535faa1c14edc39a
Egor Iskrenkov <mrblazon@gmail.com>
2018-10-14 22:24:21 +0300

    random unsplash photo func added

--- commands.py
+++ commands.py
@@ -4,4 +4,4 @@ def start(bot, update):
     bot.send_message(chat_id = update.message.chat_id, text = "I'm a bot, please talk to me!")
 
 def random_unsplash_photo(bot, update):
-	bot.send_photo(chat_id = chat_id, photo = 'https://source.unsplash.com/1600x900/')
+	bot.send_photo(chat_id = update.message.chat_id, photo = 'https://source.unsplash.com/1600x900/')
