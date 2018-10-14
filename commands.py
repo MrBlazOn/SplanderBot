3a2b70c0a5b7cd0775ba01af360d9ac293b45051
Egor Iskrenkov <mrblazon@gmail.com>
2018-10-14 23:04:53 +0300

    unsplash func fix

--- commands.py
+++ commands.py
@@ -5,9 +5,8 @@ from unsplash.auth import Auth
 client_id = "1904d81ebf366e14d1d5ce804d0f268a29126d9a18d27703ab554b92570fffe6"
 client_secret = "fc68dbac271c9450d7d157daac2ce9bb633088cc3447f1f228b74597205d842d"
 redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
-code = ""
 
-auth = Auth(client_id, client_secret, redirect_uri, code=code)
+auth = Auth(client_id, client_secret, redirect_uri, code = code)
 unsplash_api = Api(auth)
 
 def start(bot, update):
@@ -15,5 +14,5 @@ def start(bot, update):
 
 def random_unsplash_photo(bot, update):
 	random_photo = unsplash_api.photo.random()
-	print(random_photo)
-	bot.send_photo(chat_id = update.message.chat_id, photo = random_photo)
+	url = unsplash_api.photo.get(random_photo.id)
+	bot.send_photo(chat_id = update.message.chat_id, photo = url)
