9a7b796962cfb3ec4913206d3bd846e63a4e4163
TimGusev <tim.goosev@gmail.com>
2018-10-25 23:47:28 +0300

    config updated

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 token = '663108102:AAF-Vx7U_yuIDyCKY4JR5eywTsXFdmEgguU'
+client_id = "1904d81ebf366e14d1d5ce804d0f268a29126d9a18d27703ab554b92570fffe6"
+client_secret = "fc68dbac271c9450d7d157daac2ce9bb633088cc3447f1f228b74597205d842d"
+redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+code = ''
