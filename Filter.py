05a65ab2000a0e9e1069becaaa9f76fb865bf70d
Gusev <goosev.tim@gmail.com>
2018-11-17 18:55:27 +0300

    hot fix

--- Filter.py
+++ Filter.py
@@ -2,9 +2,8 @@ from telegram.ext import BaseFilter
 
 class FilterOfCommands(BaseFilter):
 	ListofCommands = ['random', 'search', 'few']
-
-    def filter(self, message):
-        if ListofCommads not in massage.text :
-           	return True
-        else:
-        	return False
+	def filter(self, message):
+		if ListofCommads not in massage.text :
+			return True
+		else:
+			return False
